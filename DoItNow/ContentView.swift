2930d44859a54838f4e3db106f608174aceba8e3
arnold therigan <arnold.therigan@bareksa.com>
2025-07-24 16:17:56 +0700

    [Onboarding]<feat>: initiate file

---
+++ DoItNow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DoItNow
+//
+//  Created by Arnold Therigan on 24/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DoItNow/DoItNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoItNowApp.swift
+//  DoItNow
+//
+//  Created by Arnold Therigan on 24/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DoItNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
